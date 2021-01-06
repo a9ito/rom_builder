3cdf2c1b29298399c4cc4bb4d3842ec8b62ed699
a9ito <anggit.febriantoro@gmail.com>
2021-01-06 14:31:26 +0700

    add some clone args

--- clone.sh
+++ clone.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 # write commands if you want to clone some repos
+git clone https://github.com/nitrogen-project/android_hardware_qcom_display -b 11-caf-sm8150 display && git clone https://github.com/nitrogen-project/android_hardware_qcom_media -b 11-caf-sm8150 media && git clone https://github.com/nitrogen-project/android_hardware_qcom_audio -b 11-caf-sm8150 audio
